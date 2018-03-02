e58ce5ea2e6d2053ce36d3e4b98c583cf359b0be
JerinBoyd <maxers@mac.com>
2018-03-02 18:35:49 -0500

    added h1 with counter and increment and decrement store and count reducer

--- src/App.js
+++ src/App.js
@@ -8,23 +8,39 @@ const initialState = 0;
 
 //reducer (how state changes)
 const countReducer = (state=initialState, action) => {
-  console.log('action');
-  return 7;
+  switch(action.type) {
+    case "INCREMENT":
+     return state + 1;
+    case 'DECREMENT':
+     return state - 1;
+    default:
+     return state 
+
+  }
 };
 
 //creation of the store
-const store = createStore(countReducer);
-debugger;
+const store = createStore(
+  countReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+);
+
+
+
 
 class App extends Component {
   render() {
     return (
       <div>
-        Hello World!
+        <h1>{store.getState()}</h1>
+        <button onClick={() => store.dispatch({type: 'INCREMENT'})}> + </button>
+        <button onClick={() => store.dispatch({type: 'DECREMENT'})}> - </button>
         
       </div>
     );
   }
 }
+
 
 export default App;

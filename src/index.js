87a8771f1138cc08b120602e7e967b5c320b0385
JerinBoyd <maxers@mac.com>
2018-03-02 20:35:16 -0500

    made store.js file and moved the countReducer into it

--- src/index.js
+++ src/index.js
@@ -4,30 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import store from './store';
 
-//initial state
-const initialState = 0;
-
-//reducer (how state changes)
-const countReducer = (state=initialState, action) => {
-  switch(action.type) {
-    case "INCREMENT":
-     return state + 1;
-    case 'DECREMENT':
-     return state - 1;
-    default:
-     return state 
-
-  }
-};
-
-//creation of the store
-const store = createStore(
-  countReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-
-);
 
 ReactDOM.render(
 <Provider store={store}>
@@ -36,3 +14,5 @@ ReactDOM.render(
 
 document.getElementById('root'));
 registerServiceWorker();
+
+
